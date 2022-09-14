5718fa700f33c64903507518d1a7c599ae7d3597
Kira Gorinchoi <voronova.kira@gmail.com>
2022-09-14 18:14:45 +0300

    test fix

--- routes/users.js
+++ routes/users.js
@@ -18,7 +18,7 @@ router.patch('/me', celebrate({
 }), patchProfile);
 router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string(),
+    avatar: Joi.string().pattern(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/),
   }),
 }), patchAvatar);
 
